d0ebbeefe06715b43137c12a5ed67264615d54af
Kad <learning.aik@gmail.com>
2024-04-24 10:57:28 +0200

    JS now handle the table building on the page load by getting the data from django without request using django template tag and filter json_script

--- djangoProject/dem1/templates/dem1/js/script.js
+++ djangoProject/dem1/templates/dem1/js/script.js
@@ -1,9 +1,10 @@
-
+const headers = ["id", "Age", "Sex", "BP", "Cholesterol", "Na_to_K", "Drug"];
 document.addEventListener('DOMContentLoaded', main);
 function main(event){
+    table_building();
     // Getting the rendered url
     const url = document.getElementById("main-table-head").dataset.url;
-    console.log(url);
+    console.log(url + " => Threw from function main");
     // Getting clickable headers
     const headers = document.getElementsByClassName("table-header-link");
     // Binding listener to headers
@@ -47,4 +48,52 @@ function process_header_click(event, element, url){
         })
         .then((data) => console.log(data))
         .catch((error) => console.error("Error during fetching : ", error));
+}
+
+function table_builder(table, headers){
+
+    //table head building
+    // Getting the url value for th columns (index page)
+    const url = document.getElementById("index-page").value;
+    console.log("URL value : " + url + " => threw from function table_builder");
+    let thead_builder = `<thead id="main-table-head" data-url=${url}> <tr>`;
+    for (let col of headers){
+        thead_builder += `<th class="table-header-link" data-header-col=${col}>` +
+            col + "</th>";
+    }
+    thead_builder += "</thead>";
+
+    //table body building
+    let tbody_builder = "<tbody>";
+    for (let element of table){
+        tbody_builder += "<tr>";
+            for (let col of headers){
+                tbody_builder += "<td>" + element[col] + "</td>";
+            }
+        tbody_builder += "</tr>";
+    }
+    tbody_builder += "</tbody>"
+    // Building the table block
+    const table_built = "<table>" +
+                                thead_builder +
+                                tbody_builder +
+                                "</table>";
+    return table_built;
+}
+
+function table_building(){
+    // query_result in an array
+    const  query_result = document.querySelector("#query_result").textContent;
+    console.log(query_result);
+    console.log(typeof(query_result));
+    console.log("Parsing ...")
+    const result_set = JSON.parse(query_result);
+    console.log(result_set);
+    console.log(typeof(result_set));
+    // Table building
+    const built_table = table_builder(result_set, headers);
+    // Table rendering
+    document.querySelector("#table-content").innerHTML = built_table;
+    console.log(built_table);
+
 }
